6f33f7678dacfff863dcfb5d9281e85b158ba7f9
Liam Martens <hi@liammartens.com>
2023-04-07 11:28:17 -0400

    feat: catchall test

--- app/[[...path]]/page.tsx
+++ app/[[...path]]/page.tsx
@@ -1,10 +1,28 @@
 import Link from 'next/link';
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import equal from 'fast-deep-equal';
+import { notFound } from 'next/navigation';
 
-const inter = Inter({ subsets: ['latin'] })
+type Params = {
+  path: string[]
+}
 
-export default function Home() {
+const allParams =  [
+  { path: [] }, // i18n.defaultLocale homepage
+  { path: ['page-1'] },
+  { path: ['page-2'] },
+];
+
+export async function generateStaticParams(): Promise<Params[]> {
+  return allParams
+}
+
+export default function Page({ params }: { params?: Params }) {
+  const isMatch = !params?.path || allParams.some((x) =>  equal(x.path, params?.path));
+
+  if (!isMatch) {
+    notFound();
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
